ba8d77e36bf495dec02964dca2f4a0f36075e716
Murilo Jose <murilojosemoreira@hotmail.com>
2021-01-28 11:36:07 -0300

    Criando Layout com Blade, Incrementando Bootstrap e Fonte

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonte do Google -->    
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+       
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+        @yield('content')
+        <footer>
+           <p>Gestor de Eventos &copy; 2021</p>
+        </footer>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    
+    $arr = [1,2,3,4,5];
+
+    return view('welcome',
+    [
+        'arr' => $arr
+    ]);
+});
+
+Route::get('/produtos', function () {
+    return view('produtos');
 });
